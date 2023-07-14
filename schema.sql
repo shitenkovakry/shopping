76d01c04d12d8f1a25cc07e31ec4680d033dd6c9
kry shitenkova <shitenkovakris@mail.ru>
2023-07-14 19:41:55 +0300

    create schema.sql

---
+++ schema.sql
@@ -0,0 +1,44 @@
+create table buyers(
+    id serial primary key,
+	name varchar(255),
+	email varchar(255),
+	balance float
+);
+
+-- нельзя запускать в sql
+insert into buyers(name, email, balance)
+values ('Ondrys', 'rianby64@mail.ru', 100.0),
+        ('Kry', 'kry@gmail.com', 500.0),
+        ('Jenya', 'yurkevich2@mail.ru', 150.5),
+        ('Marina','yukmari@mail.ru', 300.5);
+
+
+create table items(
+    id serial primary key,
+	name varchar(255),
+	price float
+);
+
+-- нельзя запускать в sql
+insert into items(name, price)
+values ('ramen', 50),
+       ('cucumber', 25),
+       ('cheese', 10),
+       ('kimchi', 26);
+
+
+create table purchases (
+    id_purchase serial primary key,
+	buyer_id int,
+	item_id int,
+	foreign key (buyer_id) references buyers(id),
+	foreign key (item_id) references items(id)
+);
+
+-- нельзя запускать в sql
+insert into purchases(buyer_id, item_id)
+values (1, 3),
+       (2, 4),
+       (4, 2),
+       (4, 1),
+       (3, 3);
